ea007be4ffcbe6e8183fe1462fdc70aec91f6bac
EugeniuPeruzzi <jaka1997@live.it>
2023-05-27 17:45:21 +0200

    adjusted 10x10 grid fixed

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ let play = document.getElementById('play');
 let boxGrid = document.querySelector('.grid-container');
 
 function gridCreator(numeri) {
-    for (let i = 1; i <= numeri; i++) {
+    for (let i = 1; i <= numeri*numeri; i++) {
 
       let box = document.createElement('div');
       box.classList.add('box');
+      box.style.width = `calc(100% / ${numeri + 2})`
       box.innerHTML = `<a href="#">${i}</a>`;
+      
       box.addEventListener('click', function() {
         box.classList.toggle('box-color');
         console.log(i)
@@ -19,4 +21,4 @@ function gridCreator(numeri) {
     }
 }
 
-gridCreator(100)
+gridCreator(10)
