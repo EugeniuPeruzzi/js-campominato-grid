1eaba5015b846c86f12582342d626eb45558f165
EugeniuPeruzzi <jaka1997@live.it>
2023-05-27 16:15:25 +0200

    fuction that generates the grid

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let grid = document.querySelector('.grid-container');
+let play = document.getElementById('play');
+let box = document.querySelector('.grid-container');
+
+function grdiCreator(numeri){
+}
+for(let i=1; i<=100; i++){
+    console.log(i);
+    box.innerHTML += `  
+    <div class="box">
+    ${i}
+    </div>`
+}
