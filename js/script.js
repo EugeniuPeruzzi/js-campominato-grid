cace6fe959bb13c9990e8c290c01486ae07938ce
EugeniuPeruzzi <jaka1997@live.it>
2023-05-28 17:06:33 +0200

    grid chioses

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ let play = document.getElementById('play');
 let boxGrid = document.querySelector('.grid-container');
 let reset  = document.getElementById('reset');
 
+
+
 function gridCreator(numeri) {
     for (let i = 1; i <= numeri*numeri; i++) {
 
       let box = document.createElement('div');
       box.classList.add('box');
-      box.style.width = `calc(100% / ${numeri + 2})`
+      box.style.width = `calc(100% / ${numeri})`
       box.innerHTML = `<a href="#">${i}</a>`;
       
       box.addEventListener('click', function() {
@@ -22,12 +24,19 @@ function gridCreator(numeri) {
     }
 }
 
-reset.addEventListener('click', function(){
-  boxGrid.innerHTML = ` `;
-});
-
+gridCreator(10)
 
 play.addEventListener('click', function(){
-  gridCreator(10)
+  let gridSelector = document.getElementById('gridselectror').value;
+  boxGrid.innerHTML = '';
+  if (gridSelector === '3'){
+    gridCreator(7)
+  }
+  else if ( gridSelector === '2'){
+    gridCreator(9)
+  }
+  else{
+    gridCreator(10)
+  }
 
 });
